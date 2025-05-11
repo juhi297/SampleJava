8789fa121e101901a8bf5ab9adb0fee33d9fa26c
juhi297 <juhigajbhiye4@gmail.com>
2025-05-11 21:51:13 +0530

    Unicode Program
    Marathi Unicode

---
+++ src/UnicodeNative.java
@@ -0,0 +1,17 @@
+
+public class UnicodeNative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        
+		        char c0 ='\u091C';
+		        char c1='\u0941';
+		        char c2='\u0939';
+		        char c3='\u0940';      
+		String marathiName ="" +c0 + c1 +c2 + c3;
+
+		        System.out.println("native name :" +marathiName);
+	}
+
+}
