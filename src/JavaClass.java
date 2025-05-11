f0088bf570e4b6727971563100c15fb3a438fa22
juhi297 <juhigajbhiye4@gmail.com>
2025-05-11 20:54:01 +0530

    last commit
    commit

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -1,16 +1,34 @@
-public class JavaClass
-{
+public class JavaClass{
 	public static void main(String[] args) {
-	int i=1;
-	int j=0;
-	if(i++ > ++j)
-	{
-		System.out.println("hello");
+		//String name;
+		//String branchLocation;
+		Bank b1=new Bank("HDFC","pune");
+		System.out.println(b1);
+		b1=null;
+		System.gc();
 	}
-	else {
-		System.out.println("welcome");
+}
+
+
+
+class Bank{
+	String name;
+	String branchLocation;
+	Bank(String name, String branchLocation){
+		this.name=name;
+		this.branchLocation=branchLocation;
 	}
-	
-	System.out.println("log" + i + ":" +j); 
+	public String toString() {
+		String bankInfo= name + "->"+branchLocation;
+		return bankInfo;
+		
+		
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stu
+		System.out.println(this.name +"is garbage collected");
+		
+		super.finalize();
 	}
 }
