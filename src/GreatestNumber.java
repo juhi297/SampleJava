5d1c9b7be75b3bdae11d5966fc5501b20b21e6be
juhi297 <juhigajbhiye4@gmail.com>
2025-06-08 16:34:30 +0530

    Display Greatest number
    Display greatest number from 3 numbers

---
+++ src/GreatestNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		 System.out.print("Enter num1 ");
+		int num1 = scanner.nextInt();  
+		System.out.print("Enter num2 ");
+		int num2 = scanner.nextInt();  
+		System.out.print("Enter num3");
+		int num3 = scanner.nextInt();  
+		if(num1>num2&&num1>num3) {
+			System.out.println("greatest number is:" +num1);
+		}
+		else
+			if(num2>num1&&num2>num3) {
+				System.out.println("greatest number is:" +num2);
+			}
+			else
+		if(num3>num2&&num3>num1) {
+			System.out.println("greatest number is:" +num3);
+		}
+			
+		
+	     
+	      
+	      
+		
+
+	}
+
+}
