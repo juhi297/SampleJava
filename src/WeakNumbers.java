dbf858c74f4257c145010e66b8f22c67e63e45bb
juhi297 <juhigajbhiye4@gmail.com>
2025-06-14 15:07:03 +0530

    Name of weekday
    program for name of weekday

---
+++ src/WeakNumbers.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class WeakNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		 System.out.print("Enter value for num1 ");
+		 int num1 = scanner.nextInt();
+		 if(num1==1) {
+			 System.out.println("Sunday");
+			 
+		 }
+		 
+			 if(num1==2) {
+				 System.out.println("Monday");
+				 
+			 }
+		 
+	
+		 if(num1==3) {
+			 System.out.println("Tuesday");
+			 
+		 }
+	 
+
+	 if(num1==4) {
+		 System.out.println("Wednesday");
+		 
+	 }
+
+	 if(num1==5) {
+		 System.out.println("Thursday");
+		 
+	 }
+
+	 if(num1==6) {
+		 System.out.println("Friday");
+		 
+	 }
+
+	 if(num1==7) {
+		 System.out.println("Saturday");
+		 
+	 }
+}
+
+	}
+
